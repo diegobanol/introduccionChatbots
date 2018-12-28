528c7cde966c8a1ae4404f0a50351ab004f97e77
Diego Alejandro Bañol <dialbari@gmail.com>
2018-12-28 13:56:29 -0500

    Delete probeTemp.py

--- probeTemp.py
+++
@@ -1,7 +0,0 @@
-import pyowm
-
-
-owm = pyowm.OWM('4bf4d2493999fbf6bf563d1f337f5fd8')
-observation = owm.weather_at_place('London,GB')
-w = observation.get_weather()
-print(w.get_temperature('celsius'))
